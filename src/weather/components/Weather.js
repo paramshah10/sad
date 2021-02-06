084368e2c0bd974795442b644716ced7beb3bf23
Param Shah <paramshah09@gmail.com>
2021-02-05 17:26:59 -0800

    removed forecasting weather app is from: https://github.com/rawfour/weather-app-react

--- src/weather/components/Weather.js
+++ src/weather/components/Weather.js
@@ -1,62 +1,14 @@
 import React from 'react';
 import styled from 'styled-components';
 import SearchCity from './SearchCity';
-import device from '../responsive/Device';
 import Result from './Result';
 import NotFound from './NotFound';
 import '../index.sass'
 
-const AppTitle = styled.h1`
-  display: block;
-  height: 64px;
-  margin: 0;
-  padding: 20px 0;
-  font-size: 20px;
-  text-transform: uppercase;
-  font-weight: 400;
-  color: #ffffff;
-  transition: 0.3s 1.4s;
-  opacity: ${({ showLabel }) => (showLabel ? 1 : 0)};
-
-  ${({ secondary }) =>
-    secondary &&
-    `
-    opacity: 1;
-    height: auto;
-    position: relative;
-    padding: 20px 0;
-    font-size: 30px;
-    top: 20%;
-    text-align: center;
-    transition: .5s;
-    @media ${device.tablet} {
-      font-size: 40px;
-    }
-    @media ${device.laptop} {
-      font-size: 50px;
-    }
-    @media ${device.laptopL} {
-      font-size: 60px;
-    }
-    @media ${device.desktop} {
-      font-size: 70px;
-    }
-    
-  `}
-
-  ${({ showResult }) =>
-    showResult &&
-    `
-    opacity: 0;
-    visibility: hidden;
-    top: 10%;
-  `}
-`;
-
 const WeatherWrapper = styled.div`
   max-width: 1500px;
   margin: 0 auto;
-  height: calc(100vh - 84px);
+  height: calc(100vh - 104px);
   width: 100%;
   position: relative;
 `;
@@ -86,21 +38,18 @@ class WeatherApp extends React.Component {
       return;
     }
 
-    localStorage.setItem('city', value)
-
     const APIkey = process.env.REACT_APP_API_KEY;
-    console.log('API key is', APIkey)
+    
     const weather = `https://api.openweathermap.org/data/2.5/weather?q=${value}&APPID=${APIkey}&units=metric`;
-    const forecast = `https://api.openweathermap.org/data/2.5/forecast?q=${value}&APPID=${APIkey}&units=metric`;
-
-    Promise.all([fetch(weather), fetch(forecast)])
-      .then(([res1, res2]) => {
-        if (res1.ok && res2.ok) {
-          return Promise.all([res1.json(), res2.json()]);
+    
+    fetch(weather)
+      .then(async (res) => {
+        if (res.ok) {
+          return await res.json();
         }
-        throw Error(res1.statusText, res2.statusText);
+        throw Error(res.statusText);
       })
-      .then(([data1, data2]) => {
+      .then((data) => {
         const months = [
           'January',
           'February',
@@ -120,29 +69,31 @@ class WeatherApp extends React.Component {
         const date = `${days[currentDate.getDay()]} ${currentDate.getDate()} ${
           months[currentDate.getMonth()]
         }`;
-        const sunset = new Date(data1.sys.sunset * 1000).toLocaleTimeString().slice(0, 5);
-        const sunrise = new Date(data1.sys.sunrise * 1000).toLocaleTimeString().slice(0, 5);
+        const sunset = new Date(data.sys.sunset * 1000).toLocaleTimeString().slice(0, 5);
+        const sunrise = new Date(data.sys.sunrise * 1000).toLocaleTimeString().slice(0, 5);
 
         const weatherInfo = {
-          city: data1.name,
-          country: data1.sys.country,
+          city: data.name,
+          country: data.sys.country,
           date,
-          description: data1.weather[0].description,
-          main: data1.weather[0].main,
-          temp: data1.main.temp,
-          highestTemp: data1.main.temp_max,
-          lowestTemp: data1.main.temp_min,
+          description: data.weather[0].description,
+          main: data.weather[0].main,
+          temp: data.main.temp,
+          highestTemp: data.main.temp_max,
+          lowestTemp: data.main.temp_min,
           sunrise,
           sunset,
-          clouds: data1.clouds.all,
-          humidity: data1.main.humidity,
-          wind: data1.wind.speed,
-          forecast: data2.list,
+          clouds: data.clouds.all,
+          humidity: data.main.humidity,
+          wind: data.wind.speed,
         };
         this.setState({
           weatherInfo,
           error: false,
         });
+        
+        localStorage.setItem('city', data.name)
+        this.props.fetchSentimentData(data.name)
       })
       .catch(error => {
         console.log(error);
